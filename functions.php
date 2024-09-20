7001b9a127b254b0949e40c30b6b704d2a28b6bf
Vikash Lohiya <62418120+vikashlohiya@users.noreply.github.com>
2024-09-20 07:46:57 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ function custom_mail($to,$username,$subject,$body) {
     $mail->Host       = 'smtp.gmail.com';      
     $mail->SMTPAuth   = true;                   
     $mail->Username   = 'vikashlohiya8716@gmail.com';
-    $mail->Password   = 'zzwoxispaxkuebll';    
+    $mail->Password   = '';    
     $mail->SMTPSecure = 'tls';                   
     $mail->Port       = 587;                     
 
